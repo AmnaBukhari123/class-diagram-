427c20ce9fd7d966b71be9c61e54e8d63b49bd6d
Amna <bukhariamna258@gmail.com>
2024-09-06 17:51:16 +0500

    Delete CarWash_Record.py

--- CarWash_Record.py
+++
@@ -1,8 +0,0 @@
-class CarWash_Record:
-    def __init__(self, worker, vehicle):
-        self.worker = worker
-        self.vehicle = vehicle
-
-    def __str__(self):
-        return f"Car Wash Record - Vehicle No : {self.vehicle.Vehicle_no_plate} washed by worker {self.worker.Worker_Id}"
-
